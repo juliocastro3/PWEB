f3ca5b477b6e45ac4ebdbeb1fa071bb099cb2ede
juliocastro3 <78832505+juliocastro3@users.noreply.github.com>
2022-11-23 22:21:40 -0300

    Add files via upload

---
+++ Atividade 12/Janela.js
@@ -0,0 +1,14 @@
+var janela = document.getElementById("janela");
+
+
+function abrirJanela(){
+	janela.src = "img/aberta.jpg";
+}
+
+function fecharJanela(){
+	janela.src = "img/fechada.jpg";
+}
+
+function quebrarJanela(){
+	janela.src = "img/quebrada.jpg";
+}
